e03a6eb17e2f08855cd644a6c3db9a78dbb94eff
Jide Oyelayo <jideoyelayo1@gmail.com>
2024-05-24 13:51:30 +0100

    made some templates

--- OrderbookSim/OrderDetail.h
+++ OrderbookSim/OrderDetail.h
@@ -1,8 +1,7 @@
-
 #pragma once
 
 #include <chrono>
-
+#include <nlohmann/json.hpp>
 #include "OrderType.h"
 #include "Side.h"
 #include "Usings.h"
@@ -10,10 +9,8 @@
 
 using timeChrono = std::chrono::system_clock::time_point;
 
-class OrderDetail
-{
+class OrderDetail {
 private:
-	// buy or sell
     OrderType _type;
     OrderId _id;
     Side _side;
@@ -24,13 +21,33 @@ private:
 public:
     OrderDetail(OrderType type, OrderId id, Side side, Price price, Quantity qty)
         : _type{ type }, _id{ id }, _side{ side }, _price{ price }, _quantity{ qty } {
-        _time = std::chrono::system_clock::now();    
-    } // setter in c++
-    
-    time_t getTime() {
+        _time = std::chrono::system_clock::now();
+    }
+
+    time_t getTime() const {
         return std::chrono::system_clock::to_time_t(_time);
     }
 
+    // Define how to serialize OrderDetail to JSON
+    friend void to_json(nlohmann::json& j, const OrderDetail& o) {
+        j = nlohmann::json{
+            {"type", o._type},
+            {"id", o._id},
+            {"side", o._side},
+            {"price", o._price},
+            {"quantity", o._quantity},
+            {"time", o.getTime()}
+        };
+    }
 
+    // Define how to deserialize OrderDetail from JSON
+    friend void from_json(const nlohmann::json& j, OrderDetail& o) {
+        o._type = j.at("type").get<OrderType>();
+        o._id = j.at("id").get<OrderId>();
+        o._side = j.at("side").get<Side>();
+        o._price = j.at("price").get<Price>();
+        o._quantity = j.at("quantity").get<Quantity>();
+        time_t time = j.at("time").get<time_t>();
+        o._time = std::chrono::system_clock::from_time_t(time);
+    }
 };
-
--- OrderbookSim/OrderDetailHistory.h
+++ OrderbookSim/OrderDetailHistory.h
@@ -1,27 +1,55 @@
 #pragma once
 
 #include <vector>
-
+#include <nlohmann/json.hpp>
 #include "OrderDetail.h"
 
-class OrderDetailHistory
-{
+class OrderDetailHistory {
 private:
-	std::vector<OrderDetail> buyHistory;
-	std::vector<OrderDetail> sellHistory;
-	void addOrderToBuyHistory(OrderType type, OrderId id, Side side, Price price, Quantity qty) {
-		OrderDetail newOrderDetail = OrderDetail(type, id, side, price, qty);
-		buyHistory.push_back(newOrderDetail);
-	}
-	void addOrderToSellHistory(OrderType type, OrderId id, Side side, Price price, Quantity qty) {
-		OrderDetail newOrderDetail = OrderDetail(type, id, side, price, qty);
-		sellHistory.push_back(newOrderDetail);
-	}
+    std::vector<OrderDetail> buyHistory;
+    std::vector<OrderDetail> sellHistory;
+
+    void addOrderToBuyHistory(OrderType type, OrderId id, Side side, Price price, Quantity qty) {
+        OrderDetail newOrderDetail(type, id, side, price, qty);
+        buyHistory.push_back(newOrderDetail);
+    }
+
+    void addOrderToSellHistory(OrderType type, OrderId id, Side side, Price price, Quantity qty) {
+        OrderDetail newOrderDetail(type, id, side, price, qty);
+        sellHistory.push_back(newOrderDetail);
+    }
+
 public:
-	OrderDetailHistory() {};
-	void addOrderToHistory(OrderType type, OrderId id, Side side, Price price, Quantity qty) {
-		if (side == Side::Buy) addOrderToBuyHistory(type, id, side, price, qty);
-		else if (side == Side::Sell) addOrderToSellHistory(type, id, side, price, qty);
-	}
+    OrderDetailHistory() = default;
+
+    void addOrderToHistory(OrderType type, OrderId id, Side side, Price price, Quantity qty) {
+        if (side == Side::Buy) {
+            addOrderToBuyHistory(type, id, side, price, qty);
+        }
+        else if (side == Side::Sell) {
+            addOrderToSellHistory(type, id, side, price, qty);
+        }
+    }
+
+    // Method to convert OrderDetailHistory to JSON
+    nlohmann::json toJSON() const {
+        return nlohmann::json{ {"buyHistory", buyHistory}, {"sellHistory", sellHistory} };
+    }
+
+    // Method to populate OrderDetailHistory from JSON
+    static OrderDetailHistory fromJSON(const nlohmann::json& j) {
+        OrderDetailHistory history;
+        j.at("buyHistory").get_to(history.buyHistory);
+        j.at("sellHistory").get_to(history.sellHistory);
+        return history;
+    }
 };
 
+// Define how to serialize and deserialize OrderDetailHistory to/from JSON
+void to_json(nlohmann::json& j, const OrderDetailHistory& history) {
+    j = history.toJSON();
+}
+
+void from_json(const nlohmann::json& j, OrderDetailHistory& history) {
+    history = OrderDetailHistory::fromJSON(j);
+}
