dc3dcbbe2a46619a21a20ede9bcbe366dd0956fc
Jide <JideOyelayo1@Gmail.com>
2024-05-27 20:13:19 +0100

    added ordertype explantions

--- OrderbookSim/OrderType.h
+++ OrderbookSim/OrderType.h
@@ -1,16 +1,16 @@
 #pragma once
 #include <string>
 enum class OrderType {
-    GoodTillCancel,
-    FillAndKill,
-    Market,
-    GoodForDay,
-    FillOrKill
+    GoodTillCancel, /* Stays active unless either filled or explicitly canceled by the trader i.e. calling cancel order*/
+    FillAndKill, /*attempy to fill as much as possible right now and cancel the remaining*/
+    Market, /*attempt to get at the best avaiable current price*/
+    GoodForDay, /* remains active until the end of the trading day - if not filled is automatically cancelled*/
+    FillOrKill /* no partial filling. it will either completely fill or cancel*/
 };
 
 inline std::string orderTypeToString(OrderType type) {
     switch (type) {
-    case OrderType::GoodTillCancel: return "GoodTillCancel";
+    case OrderType::GoodTillCancel: return "GoodTillCancel"; 
     case OrderType::FillAndKill: return "FillAndKill";
     case OrderType::Market: return "Market";
     case OrderType::GoodForDay: return "GoodForDay";
